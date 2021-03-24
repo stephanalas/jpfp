766b5d4d19be861d2163bc348ff04eb578b11a6f
Stephan Alas <stephan.j.alas@gmail.com>
2021-03-24 07:48:14 -0400

    add forms made

---
+++ client/components/AddCampusForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createCampus } from '../store';
+
+class AddCampusForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      address: ''
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  async onSubmit(ev) {
+    ev.preventDefault();
+    try {
+      await this.props.create(this.state.name, this.state.address);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  onChange(ev) {
+    const change = {};
+    change[ev.target.name] = ev.target.value;
+    change[ev.target.address] = ev.target.value;
+    this.setState(change);
+  }
+  render() {
+    const { state: {name, address}, onChange, onSubmit } = this;
+    return (
+      <div>
+        <h1>Campus Name</h1>
+        <form onSubmit={onSubmit}>
+          <label htmlFor={name} >Name:<input name='name' id='name' value={name} onChange={onChange} ></input></label>
+          <label htmlFor={address} >Address:<input name='address' id='address' value={address} onChange={onChange} ></input></label>
+          
+          <button className='add-campus-btn'>Add Campus</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(null, (dispatch, { history }) => {
+  return {
+    create: (name, address) => dispatch(createCampus(name, address, history))
+  }
+})(AddCampusForm);
---
+++ client/components/AddStudentForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createCampus, createStudent } from '../store';
+
+class AddStudentForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName : '',
+      lastName: '',
+      email: '',
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  async onSubmit(ev) {
+    ev.preventDefault();
+    const { firstName, lastName, email } = this.state
+    try {
+      await this.props.create(firstName, lastName, email);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  onChange(ev) {
+    this.setState({[ev.target.name]: ev.target.value})
+  }
+  render() {
+    const { state: {firstName, lastName, email}, onChange, onSubmit } = this;
+    return (
+      <div>
+        <h1>Enter student info</h1>
+        <form onSubmit={onSubmit}>
+          <label htmlFor={firstName} >First Name:</label>
+          <input name='firstName' value={firstName} onChange={onChange} ></input>
+          <label htmlFor={lastName} >Last Name:<input name='lastName'  value={lastName} onChange={onChange} ></input></label>
+          <label htmlFor={email} >E-mail:<input name='email'  value={email} onChange={onChange} ></input></label>
+          
+          <button className='add-campus-btn'>Add Student</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(null, (dispatch, { history }) => {
+  return {
+    create: async (firstName, lastName, email) => dispatch(createStudent(firstName, lastName, email, history))
+  }
+})(AddStudentForm);
