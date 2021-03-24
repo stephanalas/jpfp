aec68f8a4b7c975638e21015f192ac511b6fe731
Stephan Alas <stephan.j.alas@gmail.com>
2021-03-24 07:45:52 -0400

    added createStudent and createCampus thunks

--- client/store.js
+++ client/store.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 // action constants
 const SET_CAMPUSES = 'SET_CAMPUSES';
 const SET_STUDENTS = 'SET_STUDENTS';
-
+const CREATE_CAMPUS = 'CREATE_CAMPUS';
+const CREATE_STUDENT = 'CREATE_STUDENT';
 // action creators
 const setCampuses = (campuses) => {
   return {
@@ -13,11 +14,22 @@ const setCampuses = (campuses) => {
     campuses
   }
 }
-
+const _createCampus = (campus) => {
+  return {
+    type: CREATE_CAMPUS,
+    campus
+  }
+}
 const setStudents = (students) => {
   return {
     type: SET_STUDENTS,
     students
+  }
+}
+const _createStudent = (student) => {
+  return {
+    type: CREATE_STUDENT,
+    student
   }
 }
 // thunk creators
@@ -27,6 +39,15 @@ export const fetchCampuses = () => {
     dispatch(setCampuses(campuses));
   }
 }
+export const createCampus = (name, address, history) => {
+  return async (dispatch) => {
+    const campus = (await axios.post('/api/campuses', {name, address} )).data;
+    dispatch(_createCampus(campus));
+    // history.push(`/campuses/${campus.id}`);
+    history.push('/campuses')
+  }
+}
+
 
 export const fetchStudents = () => {
   return async (dispatch) => {
@@ -35,11 +56,20 @@ export const fetchStudents = () => {
   }
 }
 
-
+export const createStudent = (firstName, lastName, email, history) => {
+  return async (dispatch) => {
+    const student = (await axios.post('/api/students/', {firstName, lastName, email})).data;
+    dispatch(_createStudent(student));
+    history.push('/students')
+  }
+}
+ 
 // reducers
 const campusesReducer = (state = [], action) => {
   if (action.type === SET_CAMPUSES) {
     return action.campuses;
+  } else if (action.type === CREATE_CAMPUS) {
+    state = [...state, action.campus];
   }
   return state;
 }
@@ -47,6 +77,8 @@ const campusesReducer = (state = [], action) => {
 const studentsReducer = (state = [],action) => {
   if (action.type === SET_STUDENTS) {
     return action.students;
+  } else if (action.type === CREATE_STUDENT) {
+    return [...state, action.student];
   }
   return state
 }
