7bdaa792d8db5fd7a6ec6813358c614201f82647
Stephan Alas <stephan.j.alas@gmail.com>
2021-03-28 14:41:38 -0400

    StudentCard created

---
+++ client/components/StudentCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const StudentCard = ({student, destroy}) => {
+  return (
+    <li key={student.id} className='student-card'>
+      <img src={student.imageUrl} className='student-img' />
+      <section className='student-card-info'>
+        <Link to={`/students/${ student.id }`}><h2>{ student.firstName } { student.lastName }</h2></Link>
+
+          { 
+            student.campus ? <span>Attending:  
+            <Link to={`/campuses/${ student.campus.id }`} >{ student.campus.name }</Link> </span> 
+              : 'Currently not attending a campus'
+          }
+        
+        <button onClick={() => destroy(student)} className='delete-btn'>delete</button>
+      </section>
+    </li>
+  )
+}
+
+export default connect()(StudentCard)
