4b044d91e6196fcabb1245dc0163525ce00a5fce
Stephan Alas <stephan.j.alas@gmail.com>
2021-04-04 17:45:05 -0400

    commit before turning in

--- client/components/campus/SingleCampus.js
+++ client/components/campus/SingleCampus.js
@@ -33,7 +33,6 @@ const SingleCampus = ({ campus, students, history, destroy }) => {
           </section>
         </div>
       </section>
-      {/* comment here */}
       <section className="students-on-campus">
         <div className="campus-view-sub-header">
           <h2>Students on campus</h2>
